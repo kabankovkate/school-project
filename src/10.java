e1d0e8a12d7ee45760f9e86384eac1e49a45842f
kabankovkate <kabankovkate@gmail.com>
2025-03-15 10:14:11 +0000

    Add 10.java

---
+++ src/10.java
@@ -0,0 +1,7 @@
+import java.util.Random;
+public class Main {
+	public static void main(String[] args) {
+		int x = new Random().nextInt(10);
+		System.out.println("The random number is: " + x);
+	}
+}
