06a44a0bd8fb07820924e40d8a0138498a2a702b
kabankovkate <kabankovkate@gmail.com>
2025-03-23 03:06:19 +0000

    Add 17.java

---
+++ src/17.java
@@ -0,0 +1,6 @@
+public class SimpleSchoolProject {
+    public static void main(String[] args) {
+        // Example of a simple school project
+        System.out.println("Hello, World!");
+    }
+}
