5776de9ca372d6fcb3db3c6dd809945319382cbe
kabankovkate <kabankovkate@gmail.com>
2025-03-29 19:24:54 +0000

    Add 19.java

---
+++ src/19.java
@@ -0,0 +1,11 @@
+public class SimpleSchoolProject {
+    public static void main(String[] args) {
+        // Example of adding two numbers
+        System.out.println("The result of 10 + 20 is: " + (10 + 20));
+        
+        // Another example of using loops to print numbers from 1 to 5
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
