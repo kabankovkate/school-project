7e998e517aca6f90f381de181905e0b56ac109d7
kabankovkate <kabankovkate@gmail.com>
2025-04-07 04:07:57 +0000

    Add 28.java

---
+++ src/28.java
@@ -0,0 +1,8 @@
+public class SchoolProject {
+    public static void main(String[] args) {
+        // Example of simple Java code with variables and statements
+        String message = "Hello, world!";
+        
+        System.out.println("Message: " + message);
+    }
+}
