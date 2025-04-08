d5ebcbb25c1b5fb2ff4a5337f2e48555f2e82a13
kabankovkate <kabankovkate@gmail.com>
2025-04-08 22:27:34 +0000

    Add 29.java

---
+++ src/29.java
@@ -0,0 +1,13 @@
+public class SimpleSchoolProject {
+
+    // Method to perform some operation
+    public void performOperation() {
+        System.out.println("Performing an operation");
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        // Call the performOperation method
+        performOperation();
+    }
+}
