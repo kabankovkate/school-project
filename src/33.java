ff06dbdf015cf80553d8cf4b34211ff7dbe2e54f
kabankovkate <kabankovkate@gmail.com>
2025-04-24 02:57:12 +0000

    Add 33.java

---
+++ src/33.java
@@ -0,0 +1,9 @@
+public class SimpleSchoolProject {
+    public static void main(String[] args) {
+        // Example of adding two numbers
+        int num1 = 5;
+        int num2 = 3;
+        int sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
