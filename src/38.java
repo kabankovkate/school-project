aa3375384e86af62145b0ee67fac9a86ad1da1ed
kabankovkate <kabankovkate@gmail.com>
2025-05-18 09:20:57 +0000

    Add 38.java

---
+++ src/38.java
@@ -0,0 +1,6 @@
+public class SimpleSchoolProject {
+    public static void main(String[] args) {
+        // Your Java program logic goes here
+        System.out.println("This is a simple school project.");
+    }
+}
